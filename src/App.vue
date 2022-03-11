220f984b82b46fc69e5609d023a89bf22d27f2f9
jamespurnama1 <jamespurnama1@users.noreply.github.com>
2022-03-11 12:33:01 +0700

    added closed PO page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <h1>Hi Joey</h1>
-  <div class="grid">
+  <h3 v-if="status.published.open">PO on {{ status.published.date }}</h3>
+  <h3 v-else>PO closed</h3>
+  <div v-if="status.published.open" class="grid">
     <Cards v-for="treat in treats.published" :key="treat.name" :treat="treat"/>
     <Cards v-show="preview" v-for="treat in treats.preview" :key="treat.name" :treat="treat"/>
+  </div>
+  <div v-else>
+    <p>thank you for your enthusiasm!<br>please be patient for next open PO 😁</p>
   </div>
 </template>
 
 <script>
 import Cards from './components/Cards.vue'
 import Airtable from '@/services/Airtable'
+import Status from '@/services/Status'
 
 export default {
   name: 'App',
@@ -18,6 +24,7 @@ export default {
   data() {
     return {
       airtableResponse: [],
+      open: [],
       preview: false,
     }
   },
@@ -33,6 +40,17 @@ export default {
       }
     }
 
+    async function getStatus() {
+      try {
+        const response = await Status.getStatus()
+        console.log(response)
+        self.open = response.data.records
+      } catch(err) {
+        console.log(err)
+      }
+    }
+
+    getStatus() 
     getTreats()         
   },
   computed: {
@@ -41,15 +59,28 @@ export default {
         const preview = []
         for (var i = 0; i !== this.airtableResponse.length; i++) {
           const project = {
-              name: this.airtableResponse[i].fields.Name,
-              thumb: this.airtableResponse[i].fields.Thumbnail,
-              price: this.airtableResponse[i].fields.Price,
-              stock: this.airtableResponse[i].fields.Stock,
-              notes: this.airtableResponse[i].fields.Notes,
-              show: this.airtableResponse[i].fields['Show on Page']
+            name: this.airtableResponse[i].fields.Name,
+            thumb: this.airtableResponse[i].fields.Thumbnail,
+            price: this.airtableResponse[i].fields.Price,
+            stock: this.airtableResponse[i].fields.Stock,
+            notes: this.airtableResponse[i].fields.Notes,
+            show: this.airtableResponse[i].fields['Show on Page']
           }
-            if (this.airtableResponse[i].fields['Show on Page']) published.push(project)
-            else preview.push(project)
+          if (this.airtableResponse[i].fields['Show on Page']) published.push(project)
+          else preview.push(project)
+        }
+        return { published, preview }
+    },
+    status() {
+      let published = {}
+      let preview = {}
+      for (var i = 0; i !== this.open.length; i++) {
+          const stat = {
+            open: this.open[i].fields['PO'],
+            date: this.open[i].fields['PO Date'],
+          }
+          if (this.open[i].fields.Name === 'Live') published = stat
+          else if (this.open[i].fields.Name === 'Preview') preview = stat
         }
         return { published, preview }
     }
--- src/services/Status.js
+++ src/services/Status.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 const Axios = axios.create({
-  baseURL: "https://api.airtable.com/v0/app6MGAqCRbklbDAu/Main"
+  baseURL: "https://api.airtable.com/v0/app6MGAqCRbklbDAu/Web%20Status"
 });
 
 Axios.defaults.headers.common = {'Authorization': 'Bearer '  + process.env.VUE_APP_AIRTABLEKEY}
 
 export default{
-  getTreats() {
+  getStatus() {
     return Axios.get()
   }
 }
