9776290e6015be9b49d908f935df8e8ee6f858ee
jamespurnama1 <jamespurnama1@users.noreply.github.com>
2022-04-13 19:52:31 +0700

    cart count

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,34 +1,56 @@
 <template>
+  <button @click="$router.push('/')" class="back">
+    <p>back</p>
+  </button>
   <div class="cartPage">
     <ul>
-      <li v-for="item in cartStore.items" :key="item.name">
-        {{item.name}}
-        {{item.price}}
+      <li v-for="(qty, name) in cartList" :key="name">
+        {{qty}}x
+        {{name}}
+        Rp. {{ price(name).price }}
+        <button @click="remove(name)">
+          <p>remove</p>
+        </button>
       </li>
     </ul>
   </div>
-    <!-- <button v-show="cart.length" class="cart" @click="viewCart=false">
-      <div class="dot">
-        <p>{{cart.length}}</p>
-      </div>
-      <p>Order</p>
-      <div v-show="viewCart" class="bgChange"/>
-    </button> -->
 </template>
 
 <script>
 import { mapStores } from 'pinia'
-import { useCartStore } from "@/store";
+import { useCartStore } from "@/store"
+import gsap from 'gsap'
 
 export default {
   methods: {
     delete_cookie(name) {
       document.cookie = [name, '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/; SameSite=None;', window.location.host.toString()].join('');
       // document.cookie = [name, '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/; domain=.', 'SameSite=None', window.location.host.toString()].join('');
+    },
+    price(name) {
+      return this.cartStore.items.find((item) => item.name === name)
+    },
+    remove(name) {
+      this.cartStore.items = this.cartStore.items.filter((item) => {
+        return item !== this.price(name)
+      })
     }
   },
-  mounted() {},
+  mounted() {
+    console.log(this.cartList)
+    gsap.to('body', {
+      'background color': 'white',
+      duration: 0,
+    })
+  },
   computed: {
+    cartList() {
+      const list = {}
+      this.cartStore.items.forEach((item) => {
+        list[item.name] = (list[item.name] || 0) + 1
+      })
+      return list
+    },
     ...mapStores(useCartStore)
   }
 }
@@ -36,6 +58,30 @@ export default {
 
 <style lang="scss" scoped>
 .cartPage {
+  position: relative;
+  top: 3em;
+  text-align: left;
+}
 
+.back {
+  position: absolute;
+  top: 15px;
+  left: 15px;
+}
+
+button {
+  font-family: MADE Coachella;
+  background-color: var(--primary);
+  padding: 1em 2em;
+  border: none;
+  border-radius: 10px/10px;
+  cursor: pointer;
+  color: white;
+  margin-right: auto;
+  display: block;
+
+  p {
+    margin: 0;
+  }
 }
 </style>
